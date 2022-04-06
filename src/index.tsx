6abd330749c59b3786a83dd45e91afaf92b8990f
Jon Rutter <contact@jonrutter.com>
2022-04-06 18:52:11 -0400

    Install and import fonts

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 
 import './styles/index.css';
 
+import '@fontsource/catamaran/700.css';
+import '@fontsource/fascinate-inline';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
