59bb4ee9311b402e5fe4381095649493c52749c2
Jon Rutter <contact@jonrutter.com>
2022-04-17 13:13:02 -0400

    Clear unused code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 type Props = {
   onClick: () => void;
-  ref?: React.RefObject<HTMLButtonElement>;
 };
 
-export const Button: React.FC<Props> = ({ onClick, ref, children }) => {
+export const Button: React.FC<Props> = ({ onClick, children }) => {
   return (
     <button
       onClick={onClick}
-      className="cursor-pointer bg-gradient-to-b from-white to-[#ffcc91] border-2 border-[#d38551] shadow-md rounded-xl h-[40px] my-5 px-10 outline-none focus:ring-2 focus:ring-white"
-      ref={ref}
+      className="cursor-pointer bg-gradient-to-b from-white to-[#ffcc91] border-2 border-[#d38551] shadow-md rounded-xl h-[40px] my-5 px-10 outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-white"
       autoFocus
     >
       {children}
